690ee96fbf9b13b6b9fbb715e279d814be9d7168
Parker Manning <parkerlmanning@hotmail.com>
2020-10-22 20:55:45 -0500

    added api route for chicken coop api

---
+++ routes/chicken-api-route.js
@@ -0,0 +1,18 @@
+
+
+// function to pull data from chicken-coop api
+Module.export = function () {
+	const settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://rapidapi.p.rapidapi.com/games/%7Btitle%7D?platform=pc",
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "chicken-coop.p.rapidapi.com",
+			"x-rapidapi-key": "8e8ba0a32emsh895bad159e9c555p14b6a1jsn1a8c5a1f1928"
+		}
+	};
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+	});
+}
