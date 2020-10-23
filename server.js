5a0808b612fd64967eba2bcc352593dd04447d18
Parker Manning <parkerlmanning@hotmail.com>
2020-10-22 19:44:15 -0500

    fixed some routes in serverjs

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-require("./routes/post-api-routes.js")(app);
-require("./routes/author-api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+// require("./routes/")(app);
+// require("./routes/")(app);
+// require("./routes/")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
