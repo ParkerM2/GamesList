c76d420f836e8d007921a2fe34e2c2c59a960e74
Parker <parkerlmanning@hotmail.com>
2020-10-26 18:47:59 -0500

    adding code for login/user, authentication/saving to db still needs work, check comments, added schema/db files

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,14 @@
-CREATE DATABASE IF EXISTS
+DROP DATABASE if exists game_db;
+CREATE DATABASE game_db;
+
+USE game_db;
+
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email varchar(255) NOT NULL,
+    user_name varchar(255) NOT NULL,
+    user_password varchar(255) NOT NULL,
+    user_pokemon varchar(255),
+    user_game_list varchar(255),
+    primary key (id)
+);
