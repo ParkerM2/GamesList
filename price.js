97ab8acc0e8ee454fa3bc46be76650dcba5020c6
Emilio Conde <condeemilio891@gmail.com>
2020-10-26 19:48:02 -0500

    added lop to add attributes

--- price.js
+++ price.js
@@ -7,8 +7,20 @@ $.ajax({
     method:"GET",
   }).then(function(response){
         console.log(response)
+        showResults=[]
+        //come back to this
+         //this will grab the streaming links maybe I need another loop to grab URL arry for now grab one 
+          for(var i=0;i<response.length;i++) {
+          //console.log(response.included[i].attributes.url)
+          var gameName=(response[i].internalName)
+          console.log(gameName)
+          showResults.push(gameName)
+          console.log(gameName)
+  }}) }
 
-  }) }
+
+
 
   sCheap("half-life")
-  
+
+ 
