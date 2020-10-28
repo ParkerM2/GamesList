50496dc560f8f2e78613df1ed383aab7364cbf64
Thomas Althoff <matthewalthoff1@gmail.com>
2020-10-27 19:08:20 -0500

    sent the new route

--- routes/chicken-api-route.js
+++ routes/chicken-api-route.js
@@ -1,15 +1,14 @@
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://chicken-coop.p.rapidapi.com/games/%7Btitle%7D?platform=pc",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "chicken-coop.p.rapidapi.com",
+		"x-rapidapi-key": "c23b869635mshbd93a4ffe3425ecp12d50bjsnae0f66b387be"
+	}
+}
 
-
-// function to pull data from chicken-coop api
-module.exports = function (app) {
-	const settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://rapidapi.p.rapidapi.com/games/%7Btitle%7D?platform=pc",
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "chicken-coop.p.rapidapi.com",
-			"x-rapidapi-key": "8e8ba0a32emsh895bad159e9c555p14b6a1jsn1a8c5a1f1928"
-		};
-	};
-};
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
