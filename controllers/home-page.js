7c76c700cac04abf35872e2a04398e4fc9fd711f
Parker <parkerlmanning@hotmail.com>
2020-10-30 21:44:32 -0500

    save

--- controllers/home-page.js
+++ controllers/home-page.js
@@ -1,11 +1,7 @@
 // function for sending the logged in user to the home page
 let handleHelloWorld = async (req, res,) => {
     let user = JSON.parse(JSON.stringify(req.user))
-<<<<<<< HEAD
     console.log(user, "user object right before rendering to index of handlebars, home-pagejs")
-=======
-
->>>>>>> main
     // not sure when we need to send more data to render on the home page what parameters handlebars is looking for
     return res.render("index", user);
 };
